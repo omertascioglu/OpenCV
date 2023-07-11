268b077ec1004e371d981ad832d0f82c4eae3b08
Ömer Taşçıoğlu <omertascioglu03@gmail.com>
2023-07-11 16:36:43 +0300

    Scan People

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import cv2
 
+# Load the pre-trained classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+# Create a VideoCapture object
 cap = cv2.VideoCapture(0)
 
+# Check if the camera is opened successfully
 if not cap.isOpened():
-    print("Error opening video capture")
+    print("Could not open camera")
     exit()
 
 while True:
+    # Capture a frame from the camera
     ret, frame = cap.read()
-    if ret:
-        cv2.imshow("Camera", frame)
 
-        # Capture photo when any key is pressed
-        if cv2.waitKey(1) != -1:
-            cv2.imwrite("photo.jpg", frame)
-            print("Photo captured!")
-            break
+    # Convert the frame to grayscale
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # Detect faces in the grayscale image
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        print("People")
+
+    # Display the frame with face detection
+    cv2.imshow("Face Detection", frame)
+
+    # Press `q` to quit
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+# Release the camera
 cap.release()
+
+# Close all windows
 cv2.destroyAllWindows()
