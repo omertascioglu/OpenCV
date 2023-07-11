52b9467bea3c2523f07080939898da86a50e6c41
Ömer Taşçıoğlu <omertascioglu03@gmail.com>
2023-07-11 11:55:47 +0300

    Capture Photo from webcam connected to Computer.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
 print("Package imported")
 
-image = cv2.imread("Resources/jaiyk.png")  # Read the image
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+cap = cv2. VideoCapture (0)
+cap. set (3,640)
+cap. set (4,480)
 
-cv2.imshow("Grayscale Image", gray_image)  # Display the grayscale image
-print(gray_image.shape)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+while True:
+    success, img = cap.read()
+    cv2. imshow("Video",img)
+    if cv2.waitKey (0):
+        break
